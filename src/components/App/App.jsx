2c59819750cebabec9f5e7f49acc7d8d4682cbc8
Aleksandr Goldshtern <alexg020573@gmail.com>
2025-04-05 14:31:24 +0300

    feat: add navigation menu in the header; create About comonent

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "../Header/Header";
+import About from "../About/About";
 
 function App() {
   return (
     <div className="page">
       <div className="page__content">
         <Header />
+        <About />
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import "./Header.css";
 import logo from "../../assets/shtern-logo.png";
 function Header() {
+  const today = new Date();
   return (
     <header className="header">
       <div className="header__logo-and-title">
         <img className="header__logo" src={logo} alt="Inna Shtern logo" />
         <div className="header__title-container">
           <h1 className="header__title">Inna Shtern</h1>
-          <time className="header__date" dateTime={new Date().toISOString()}>
-            {new Date().toLocaleString("default", {
+          <time className="header__date" dateTime={today.toISOString()}>
+            {today.toLocaleString("default", {
               weekday: "long",
               month: "long",
               day: "numeric",
@@ -17,6 +18,17 @@ function Header() {
           </time>
         </div>
       </div>
+      <nav className="header__nav">
+        <a href="#about" className="header__nav-link">
+          Обо мне
+        </a>
+        <a href="#services" className="header__nav-link">
+          Услуги
+        </a>
+        <a href="#contact" className="header__nav-link">
+          Контакты
+        </a>
+      </nav>
     </header>
   );
 }
